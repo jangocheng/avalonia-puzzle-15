dacf8660904ffda3127338c93bb23b29e23661bb
Alexander Kozlenko <alexander.kozlenko@live.com>
2018-03-17 18:44:07 +0200

    Use XML namespace for local UI components

---
+++ src/Avalonia.Examples.PuzzleFifteen/Properties/Assembly.cs
@@ -0,0 +1,4 @@
+﻿using Avalonia.Metadata;
+
+[assembly: XmlnsDefinition("urn:application.ui", "Avalonia.Examples.PuzzleFifteen.Controls")]
+[assembly: XmlnsDefinition("urn:application.ui", "Avalonia.Examples.PuzzleFifteen.Converters")]
